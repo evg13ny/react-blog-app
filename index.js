fe60fa02c08c37dad21886a64afd95cbe140d267
Evgeny Polyakov <evg_pol@yahoo.com>
2022-11-25 13:05:22 +0300

    refactored backend

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import multer from 'multer'
 
-import * as PostController from './controllers/PostController.js'
-import * as UserController from './controllers/UserController.js'
-import checkAuth from './utils/checkAuth.js'
+import { PostController, UserController } from './controllers/index.js'
+import { checkAuth, handleValidationErrors } from './utils/index.js'
 import { loginValidation, postCreateValidation, registerValidation } from './validations.js'
 
 mongoose
@@ -32,10 +31,10 @@ app.use(express.json()) // express handles json
 app.use('/uploads', express.static('uploads')) // express opens uploads folder
 
 /* Authorization form */
-app.post('/auth/login', loginValidation, UserController.login)
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
 
 /* Registration form */
-app.post('/auth/register', registerValidation, UserController.register)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
 
 /* Check me */
 app.get('/auth/me', checkAuth, UserController.getMe)
@@ -49,9 +48,9 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
-app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update)
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
 
 app.listen(4444, (err) => { // server 4444
     if (err) {
